99e49e422b958573c2c1abac6e1c25a515755cc4
Abuzar <abuzarkhan1242@gmail.com>
2023-09-24 13:37:51 +0500

    HackerRank q1 Solved

---
+++ HackerRankq1.py
@@ -0,0 +1,8 @@
+a = int(input("Please Enter Value Of a: "))
+b = int(input("Please Enter Value Of b: "))
+
+print("Sum Of A and B = ", a + b)
+
+print("Subtraction of A and B " , a - b)
+
+print("Multiplication Of A and B" , a * b)
