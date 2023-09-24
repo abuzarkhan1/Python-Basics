4629a93e20d609a92439438612cb2afe4766aacb
Abuzar <abuzarkhan1242@gmail.com>
2023-09-24 13:57:50 +0500

    Task 3 solved

---
+++ Task3.py
@@ -0,0 +1,8 @@
+a = int(input("Please Enter Value Of a "))
+b = int(input("Please Enter Value Of b "))
+
+integerResult = a // b
+FloatResult = a / b
+
+print("IntgerResult = " , integerResult)
+print("FloatResult = " , FloatResult)
