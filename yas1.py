03e622f2cb3f5943f6b87575db6162073ca41018
Abuzar <abuzarkhan1242@gmail.com>
2023-10-17 21:51:16 +0500

    Updated

--- yas1.py
+++ yas1.py
@@ -1,16 +1,16 @@
 email = input('Enter your email\n')
 
-if email == 'abuzargmail.com' and '@' in email:
+if email == 'kahngmail.com' and '@' in email:
     print('Correct email')
     password = input('Enter your password\n')
     
-    if password == '1122233':
+    if password == 'khan':
         print('Finally Correct password welcome')
     else:
         print('Incorrect password. Please re-enter your password.')
         password = input('Enter your password again\n')
         
-        if password == '11222233':
+        if password == 'kna':
             print('Finally Correct password welcome')
         else:
             print('Incorrect password. Too many attempts. Please try again later.')
