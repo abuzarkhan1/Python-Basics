088bf5844d0af670f0e5f667185119fb4fed636c
Abuzar <abuzarkhan1242@gmail.com>
2023-10-18 20:15:06 +0500

    updated

--- yas1.py
+++ yas1.py
@@ -1,6 +1,6 @@
 email = input('Enter your email\n')
 
-if email == 'kahngmail.com' and '@' in email:
+if email == 'abuzargmail.com' and '@' in email:
     print('Correct email')
     password = input('Enter your password\n')
     
